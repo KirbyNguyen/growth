f7ed405836ae253e1fcb70b3d34aa14bc4b3f771
Duc Nguyen <ducnguyen@Kirbys-MBP.lan>
2022-01-01 23:53:37 -0800

    Added updated and new packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:growth/constants/nav_routes.dart';
+void main() => runApp(const MyApp());
 
-import 'package:growth/providers/app_theme_provider.dart';
-import 'package:growth/providers/shared_preferences_provider.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final sharedPreferences = await SharedPreferences.getInstance();
-  await Firebase.initializeApp();
-
-  runApp(ProviderScope(
-    overrides: [
-      // override the previous value with the new object
-      sharedPreferencesProvider.overrideWithValue(sharedPreferences),
-    ],
-    child: const MyApp(),
-  ));
-}
-
-class MyApp extends HookWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final _useAppTheme = useProvider(appThemeProvider);
-    final _useAppThemeState = useProvider(appThemeStateProvider);
-
     return MaterialApp(
-      title: "Growth",
-      theme: _useAppTheme.getThemeData(context, _useAppThemeState),
-      routes: NavigationRoutes.navRoutes,
-      initialRoute: NavigationRoutes.blankRoute,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
--- lib/models/account_db_helper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:growth/models/balance_account.dart';
-
-abstract class AccountDBHelper {
-  Future<List<BalanceAccount>> getAll(String userId);
-  Future<BalanceAccount> getOne(String id);
-  Future insert(BalanceAccount account);
-  Future update(BalanceAccount account);
-  Future delete(String id);
-}
